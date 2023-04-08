ba81fe7e3663adcee09bd6e2176e67d6b49d85e9
Gerasimov Alex <geras@hismith.ru>
2023-04-08 19:08:26 +0500

    add element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import locale from 'element-plus/lib/locale/lang/ru'
 
 const app = createApp(App)
