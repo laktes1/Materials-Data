d76f589dcbe1d74411de6976f19e0325efd432ea
Gerasimov Alex <alexandr041299@gmail.com>
2023-04-16 16:33:14 +0500

    feat: add sidebar menu

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-    <el-header height="100">
-        <h1>Materials Data</h1>
-    </el-header>
-    <el-main>
-        <router-view></router-view>
-    </el-main>
-    <!--        <el-footer>-->
-    <!--            Footer-->
-    <!--        </el-footer>-->
+    <el-container>
+        <el-header height="100">
+            <h1>Materials Data</h1>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">
+                <side-bar-menu></side-bar-menu>
+            </el-aside>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script setup lang="ts">
+import SideBarMenu from "@/components/SideBarMenu.vue";
 
 </script>
 
---
+++ src/components/SideBarMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        :router="true"
+    >
+        <el-menu-item index="1" route="/cifPage">
+            <span class="side-bar-menu__title" slot="title">CIF файлы</span>
+        </el-menu-item>
+        <el-menu-item index="2" route="/outPage">
+            <span class="side-bar-menu__title" slot="title">OUT файлы</span>
+        </el-menu-item>
+        <el-menu-item index="3" route="/filesPage">
+            <span class="side-bar-menu__title" slot="title">Все файлы</span>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.side-bar-menu {
+    &__title {
+        font-size: 16px;
+    }
+}
+</style>
