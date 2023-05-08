45e95acef1c927ce4a6cad9361b7b3f1e932016a
Gerasimov Alex <alexandr041299@gmail.com>
2023-05-08 17:44:02 +0500

    feat: add file upload to other files page

--- src/pages/Files.vue
+++ src/pages/Files.vue
@@ -25,6 +25,69 @@
         <el-row class="m-t-15" align="middle" justify="end">
             <el-button type="primary" :icon="Search" size="large">Поиск</el-button>
         </el-row>
+
+        <el-row class="m-t-15">
+            <el-collapse class="collapse-container">
+
+                <el-collapse-item name="Загрузка-файлов">
+                    <template #title>
+                        <p class="collapse-header">
+                            Загрузка файлов
+                        </p>
+                    </template>
+
+                    <!--                        <el-upload-->
+                    <!--                            v-model:file-list="fileList"-->
+                    <!--                            ref="uploadRef"-->
+                    <!--                            class="upload-demo m-t-15"-->
+                    <!--                            multiple-->
+                    <!--                            :auto-upload="false"-->
+                    <!--                            :action="fileApiActionUrl"-->
+                    <!--                            v-loading="fileUploading"-->
+                    <!--                        >-->
+                    <!--                            <template #trigger>-->
+                    <!--                                <el-button type="primary" class="m-r-15">Загрузить файлы</el-button>-->
+                    <!--                            </template>-->
+                    <!--                            <div class="el-upload el-upload&#45;&#45;text">-->
+                    <!--                                <el-button type="success" @click="handleUpload" class="m-r-15">-->
+                    <!--                                    Отправить на сервер-->
+                    <!--                                </el-button>-->
+                    <!--                            </div>-->
+                    <!--                            <div class="el-upload el-upload&#45;&#45;text">-->
+                    <!--                                <el-button type="warning" @click="clearFiles" class="m-r-15">-->
+                    <!--                                    Убрать файлы-->
+                    <!--                                </el-button>-->
+                    <!--                            </div>-->
+                    <!--                        </el-upload>-->
+
+                    <!--TODO mock данные, потом удалить-->
+                    <el-upload
+                        v-model:file-list="fileList"
+                        ref="uploadRef"
+                        class="upload-demo m-t-15"
+                        multiple
+                        :auto-upload="false"
+                        :action="mockUploadUrl"
+                        v-loading="fileUploading"
+                    >
+                        <template #trigger>
+                            <el-button type="primary" class="m-r-15">Загрузить файлы</el-button>
+                        </template>
+                        <div class="el-upload el-upload--text">
+                            <el-button type="success" @click="handleUpload" class="m-r-15">
+                                Отправить на сервер
+                            </el-button>
+                        </div>
+                        <div class="el-upload el-upload--text">
+                            <el-button type="warning" @click="clearFiles" class="m-r-15">
+                                Убрать файлы
+                            </el-button>
+                        </div>
+                    </el-upload>
+
+                </el-collapse-item>
+            </el-collapse>
+        </el-row>
     </el-card>
 
     <el-card class="m-t-15">
@@ -37,9 +100,50 @@
 <script setup lang="ts">
 import { Search } from '@element-plus/icons-vue'
 import { ref, reactive } from 'vue'
+import { Api } from '@/api'
+import {UploadInstance, UploadUserFile} from "element-plus";
+import axios from "axios";
 
 const fileName = ref('')
 const fileExtension = ref('')
+
+const uploadRef = ref<UploadInstance>()
+const fileList = ref<UploadUserFile[]>([])
+const fileApiActionUrl = `${import.meta.env.VITE_APP_API_URL}/upload_file/`
+const fileUploading = ref(false)
+
+const mockUploadUrl = ref('https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15')
+
+const handleUpload = () => {
+    if (fileUploading.value) {
+        return
+    }
+    fileUploading.value = true
+
+    const formData = new FormData()
+    fileList.value.forEach((file, id) => {
+        formData.append(`file-${id}`, file.raw)
+    })
+
+
+    // Api.Other.uploadFile(formData)
+    // TODO Mock-Данные удалить потом
+    axios.post(mockUploadUrl.value, formData)
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+        .finally(() => {
+            fileUploading.value = false // TODO сделать очистку загруженных файлов и оставить ошибочные
+        })
+}
+
+const clearFiles = () => {
+    // uploadRef.value!.submit()
+    uploadRef.value!.clearFiles()
+}
 
 const mockObj = reactive({
     'Идентификатор': 'c5434530fs-sdas-hfdghf',
