d14de4d93e856f8203a9cbd992014bada5a463f4
Gerasimov Alex <alexandr041299@gmail.com>
2023-05-22 21:41:48 +0500

    feat: close filePage in menu

--- src/components/SideBarMenu.vue
+++ src/components/SideBarMenu.vue
@@ -10,14 +10,15 @@
         <el-menu-item index="2" route="/outPage">
             <span class="side-bar-menu__title" slot="title">OUT файлы</span>
         </el-menu-item>
-        <el-menu-item index="3" route="/filesPage">
-            <span class="side-bar-menu__title" slot="title">Все файлы</span>
-        </el-menu-item>
+<!--        //TODO разкоментить, когда будет готова /filesPage-->
+<!--        <el-menu-item index="3" route="/filesPage">-->
+<!--            <span class="side-bar-menu__title" slot="title">Все файлы</span>-->
+<!--        </el-menu-item>-->
     </el-menu>
 </template>
 
 <script setup>
-
+// TODO фикс default-active="1" на значение в зависимости от текущего роута
 </script>
 
 <style scoped lang="scss">
